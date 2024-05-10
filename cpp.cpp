6b462027dc3142358de18be7425e804577c66a8e
Your Name <you@example.com>
2024-05-10 15:36:12 +0300

    commit

--- cpp.cpp
+++ cpp.cpp
@@ -1,6 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+
+//count question 
+int main()
+{
+    int count = 0;
+    int n = 3456;
+    while (n > 0)
+    {
+        count += 1;
+        n = n / 10;
+    }
+    cout << count;
 }

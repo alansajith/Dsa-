9d903127d9de39aefd394ca70d5d09562f3931bb
Your Name <you@example.com>
2024-05-10 17:50:14 +0300

    commit

--- cpp.cpp
+++ cpp.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// count no . of digits
+//  int main()
+//  {
+//      int count = 0;
+//      int n = 3456;
+//      while (n > 0)
+//      {
+//          count += 1;
+//          n = n / 10;
+//      }
+//      cout << count;
+//  }
 
-//count question 
+// reverse a number
 int main()
 {
-    int count = 0;
-    int n = 3456;
+    int n;
+    cout << "enter a number: ";
+    cin >> n;
+    int rev_num = 0;
     while (n > 0)
     {
-        count += 1;
+        int ld = n % 10;
+        rev_num = (rev_num * 10) + ld;
         n = n / 10;
     }
-    cout << count;
+    cout << rev_num;
 }

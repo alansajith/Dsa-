e35ca41ef3a7236ff36cb7c77c18cc8fb3b43704
Your Name <you@example.com>
2024-05-11 16:27:48 +0300

    commit

--- cpp.cpp
+++ cpp.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
-int main(){
-    cout<<"hi";
-    
-=======
 // count no . of digits
 //  int main()
 //  {
@@ -19,19 +14,27 @@ int main(){
 //      cout << count;
 //  }
 
-// reverse a number
-int main()
-{
-    int n;
-    cout << "enter a number: ";
-    cin >> n;
-    int rev_num = 0;
-    while (n > 0)
-    {
-        int ld = n % 10;
-        rev_num = (rev_num * 10) + ld;
-        n = n / 10;
-    }
-    cout << rev_num;
->>>>>>> 9d903127d9de39aefd394ca70d5d09562f3931bb
-}
+// reverse a number and check if its a palindrome
+// int main()
+// {
+//     int n;
+//     cout << "enter a number: ";
+//     cin >> n;
+//     int a = n;
+//     int rev_num = 0;
+//     while (n > 0)
+//     {
+//         int ld = n % 10;
+//         rev_num = (rev_num * 10) + ld;
+//         n = n / 10;
+//     }
+//     cout << rev_num << endl;
+//     if (rev_num == a)
+//     {
+//         cout << "its a palindrome";
+//     }
+//     else
+//     {
+//         cout << "its not a palindrome";
+//     }
+// }
